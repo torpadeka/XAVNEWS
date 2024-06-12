9904e5fbdf37600800d88bc583ec6beb95b376d6
BuildTools <unconfigured@null.spigotmc.org>
2024-06-12 21:44:26 +0700

    Finished Editorial Prototype Pages, fixed assets name mismatch and added finishing touches in general

--- Scripts/GlobalScript.js
+++ Scripts/GlobalScript.js
@@ -8,12 +8,47 @@ logo.addEventListener("click", function () {
     location.href = "../Views/LandingPage.html";
 });
 
-searchInput.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-        location.href = "../Views/SearchPage.html?search=" + searchInput.value;
-    }
-});
+if (searchInput != null) {
+    searchInput.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            location.href =
+                "../Views/SearchPage.html?search=" + searchInput.value;
+        }
+    });
+}
 
-filterButton.addEventListener("click", function () {
-    location.href = "../Views/CategoriesPage.html";
-});
+if (filterButton != null) {
+    filterButton.addEventListener("click", function () {
+        location.href = "../Views/CategoriesPage.html";
+    });
+}
+
+const userProfile = document.getElementById("profile-image");
+const staffProfile = document.getElementById("staff-image");
+
+if (userProfile != null) {
+    userProfile.addEventListener("click", function () {
+        location.href = "../Editorial/SubmitArticlePage.html";
+    });
+}
+
+if (staffProfile != null) {
+    staffProfile.addEventListener("click", function () {
+        location.href = "../Views/HomePage.html";
+    });
+}
+
+const submitArticleButton = document.getElementById("submit-article-button");
+const manageArticleButton = document.getElementById("manage-article-button");
+
+if (submitArticleButton != null) {
+    submitArticleButton.addEventListener("click", function () {
+        location.href = "../Editorial/SubmitArticlePage.html";
+    });
+}
+
+if (manageArticleButton != null) {
+    manageArticleButton.addEventListener("click", function () {
+        location.href = "../Editorial/ManageArticlePage.html";
+    });
+}
